302ac9ed79187251b92fdf075f58eb2bf77aed0a
bryanadams <bryan.adams@fbinv.com>
2024-07-08 21:25:53 -0600

    add NavHost with TypeSafe navigation

--- app/src/main/java/com/bryankeltonadams/fetchtakehometest/MainActivity.kt
+++ app/src/main/java/com/bryankeltonadams/fetchtakehometest/MainActivity.kt
@@ -4,10 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.bryankeltonadams.fetchtakehometest.ui.theme.FetchTakeHomeTestTheme
+import kotlinx.serialization.Serializable
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,10 +20,28 @@ class MainActivity : ComponentActivity() {
         setContent {
             FetchTakeHomeTestTheme {
                 // implement a Sticky Header for each group
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
 
+                // this box is to hold the NavHost and the SnackbarHost
+                // it is common to have a single Scaffold which holds the NavHost and SnackbarHost
+                // however, I prefer to have each screen have it's own Scaffold so that it can
+                // have it's own TopAppBar.
+                // I'm comfortable with either implementation though.
+                Box(modifier = Modifier.fillMaxSize()) {
+                    NavHost(
+                        navController = rememberNavController(),
+                        // Compose Navigation Beta allows for Type Safe Routing instead of using strings
+                        startDestination = ScreenA
+                    ) {
+                        composable<ScreenA> {
+
+
+                        }
+                    }
                 }
             }
         }
     }
 }
+
+@Serializable
+object ScreenA
