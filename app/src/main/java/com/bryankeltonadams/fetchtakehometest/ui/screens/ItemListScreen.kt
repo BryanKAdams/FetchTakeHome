66ce2dbf4f2a059a9980e03cd82c5ecc2a1fbf93
bryanadams <bryan.adams@fbinv.com>
2024-07-09 11:50:35 -0600

    add comments and string resource usages

--- app/src/main/java/com/bryankeltonadams/fetchtakehometest/ui/screens/ItemListScreen.kt
+++ app/src/main/java/com/bryankeltonadams/fetchtakehometest/ui/screens/ItemListScreen.kt
@@ -3,7 +3,6 @@ package com.bryankeltonadams.fetchtakehometest.ui.screens
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeDrawingPadding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -11,8 +10,8 @@ import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.ListItem
-import androidx.compose.material3.ListItemColors
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.pulltorefresh.PullToRefreshBox
@@ -20,9 +19,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.unit.dp
+import androidx.compose.ui.res.stringResource
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.bryankeltonadams.fetchtakehometest.R
 import com.bryankeltonadams.fetchtakehometest.data.model.Item
 
 @Composable
@@ -53,6 +52,10 @@ fun ItemListScreen(
     // this new PullToRefreshBox on the material3 beta is much better
 
     PullToRefreshBox(isRefreshing = itemListScreenUiState.isRefreshing, onRefresh = onRefresh) {
+        // I could have put the conditional logic inside the lazy column and created
+        // single box items for the progress indicator and the error message
+        // but I prefer having the other conditional parts of the ui outside the lazy column
+        // this does require having to pass in the scroll state to the boxes though
         if (itemListScreenUiState.isLoading) {
             Box(
                 modifier = Modifier
@@ -81,22 +84,49 @@ fun ItemListScreen(
                 sectionedItems?.forEach {
                     // create a sticky header for each listId
                     stickyHeader {
+                        HorizontalDivider()
                         ListItem(headlineContent = {
                             Text(
                                 text = "List ${it.key}",
                                 style = MaterialTheme.typography.titleLarge
                             )
                         })
+                        HorizontalDivider()
                     }
                     // go through the items in the map and create a ListItem for each
                     items(it.value) { item ->
                         ListItem(
                             headlineContent = {
-                                Text("Item ID:  ${item.id}")
+                                item.id?.let { itemId ->
+                                    Text(
+                                        stringResource(
+                                            id = R.string.list_item_headline_text,
+                                            itemId
+                                        )
+                                    )
+                                }
                             },
                             supportingContent = {
-                                Text("Item Name:  ${item.name}")
-                            })
+                                item.name?.let { itemName ->
+                                    Text(
+                                        stringResource(
+                                            id = R.string.list_item_supporting_text,
+                                            itemName
+                                        )
+                                    )
+                                }
+                            },
+                            // sort of redundant, but for the sake of displaying
+                            // all of the data within the item.
+                            trailingContent = {
+                                Text(
+                                    stringResource(
+                                        id = R.string.list_item_trailing_text,
+                                        item.listId
+                                    )
+                                )
+                            }
+                        )
                     }
                 }
             }
