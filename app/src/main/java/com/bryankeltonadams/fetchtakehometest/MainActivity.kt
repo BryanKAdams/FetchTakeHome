b4d849f703d033680edc3b63d43449149e1e4c89
bryanadams <bryan.adams@fbinv.com>
2024-07-08 21:48:00 -0600

    switch to single Scaffold for simplicity, since this simple project doesn't need any of the pros that come with multi scaffold. implement dummy list and display it.

--- app/src/main/java/com/bryankeltonadams/fetchtakehometest/MainActivity.kt
+++ app/src/main/java/com/bryankeltonadams/fetchtakehometest/MainActivity.kt
@@ -4,9 +4,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.safeDrawingPadding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.ListItem
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
@@ -14,6 +23,7 @@ import com.bryankeltonadams.fetchtakehometest.ui.theme.FetchTakeHomeTestTheme
 import kotlinx.serialization.Serializable
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalFoundationApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -26,15 +36,57 @@ class MainActivity : ComponentActivity() {
                 // however, I prefer to have each screen have it's own Scaffold so that it can
                 // have it's own TopAppBar.
                 // I'm comfortable with either implementation though.
-                Box(modifier = Modifier.fillMaxSize()) {
+                Surface {
                     NavHost(
                         navController = rememberNavController(),
                         // Compose Navigation Beta allows for Type Safe Routing instead of using strings
-                        startDestination = ScreenA
+                        startDestination = ItemList
                     ) {
-                        composable<ScreenA> {
-
-
+                        composable<ItemList> {
+                            val dummyItems = listOf(
+                                Item(id = 755, listId = 2, name = ""),
+                                Item(id = 203, listId = 2, name = ""),
+                                Item(id = 684, listId = 1, name = "Item 684"),
+                                Item(id = 276, listId = 1, name = "Item 276"),
+                                Item(id = 736, listId = 3, name = null),
+                                Item(id = 926, listId = 4, name = null),
+                                Item(id = 808, listId = 4, name = "Item 808"),
+                                Item(id = 599, listId = 1, name = null),
+                                Item(id = 424, listId = 2, name = null),
+                                Item(id = 444, listId = 1, name = ""),
+                                Item(id = 809, listId = 3, name = null),
+                                Item(id = 293, listId = 2, name = null),
+                                Item(id = 510, listId = 2, name = null),
+                                Item(id = 680, listId = 3, name = "Item 680"),
+                                Item(id = 231, listId = 2, name = null),
+                                Item(id = 534, listId = 4, name = "Item 534"),
+                            )
+                            val sortedItems = dummyItems.sortedBy { it.name }
+                            val groupedItems = sortedItems.groupBy { it.listId }.toSortedMap(
+                                compareBy { it }
+                            )
+                            LazyColumn(
+                                modifier = Modifier
+                                    .safeDrawingPadding()
+                                    .fillMaxSize()
+                            ) {
+                                groupedItems.forEach {
+                                    stickyHeader {
+                                        ListItem(headlineContent = {
+                                            Text(
+                                                text = "List ${it.key}",
+                                                style = MaterialTheme.typography.titleLarge
+                                            )
+                                        })
+                                    }
+                                    items(it.value) { item ->
+                                        ListItem(headlineContent = {
+                                            Text("Item ${item.id} - ${item.name}")
+                                        }
+                                        )
+                                    }
+                                }
+                            }
                         }
                     }
                 }
@@ -43,5 +95,11 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+data class Item(
+    val id: Int? = null,
+    val name: String? = null,
+    val listId: Int? = null,
+)
+
 @Serializable
-object ScreenA
+object ItemList
