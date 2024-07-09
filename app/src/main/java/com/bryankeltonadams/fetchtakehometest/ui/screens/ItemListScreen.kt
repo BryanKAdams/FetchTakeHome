2295cc55b4fa6d73b7388f110b64a0f9c84b076b
bryanadams <bryan.adams@fbinv.com>
2024-07-09 11:00:19 -0600

    restructured ListItem

--- app/src/main/java/com/bryankeltonadams/fetchtakehometest/ui/screens/ItemListScreen.kt
+++ app/src/main/java/com/bryankeltonadams/fetchtakehometest/ui/screens/ItemListScreen.kt
@@ -3,6 +3,7 @@ package com.bryankeltonadams.fetchtakehometest.ui.screens
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeDrawingPadding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -11,6 +12,7 @@ import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ListItem
+import androidx.compose.material3.ListItemColors
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.pulltorefresh.PullToRefreshBox
@@ -18,16 +20,16 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.bryankeltonadams.fetchtakehometest.data.model.Item
 
 @Composable
 fun ItemListScreen(itemListScreenViewModel: ItemListScreenViewModel) {
-    val uiState by
-    itemListScreenViewModel.itemListScreenUiState.collectAsStateWithLifecycle()
+    val uiState by itemListScreenViewModel.itemListScreenUiState.collectAsStateWithLifecycle()
     ItemListScreen(
-        itemListScreenUiState = uiState,
-        onRefresh = itemListScreenViewModel::refresh
+        itemListScreenUiState = uiState, onRefresh = itemListScreenViewModel::refresh
     )
 }
 
@@ -41,13 +43,13 @@ data class ItemListScreenUiState(
 @OptIn(ExperimentalFoundationApi::class, ExperimentalMaterial3Api::class)
 @Composable
 fun ItemListScreen(
-    itemListScreenUiState: ItemListScreenUiState,
-    onRefresh: () -> Unit
+    itemListScreenUiState: ItemListScreenUiState, onRefresh: () -> Unit
 ) {
     val sectionedItems = itemListScreenUiState.sectionedItems
 
     // I had a great discussion with multiple guys in the Kotlin Slack channel
-    // about the production implementation of PullToRefresh and how bad it is
+    // about the production implementation of PullToRefresh and how it could use some work
+    // and how it was a step back from the material 2 implementation
     // this new PullToRefreshBox on the material3 beta is much better
 
     PullToRefreshBox(isRefreshing = itemListScreenUiState.isRefreshing, onRefresh = onRefresh) {
@@ -66,8 +68,7 @@ fun ItemListScreen(
                     .verticalScroll(rememberScrollState())
             ) {
                 Text(
-                    modifier = Modifier.align(Alignment.Center),
-                    text = itemListScreenUiState.error
+                    modifier = Modifier.align(Alignment.Center), text = itemListScreenUiState.error
                 )
             }
 
@@ -89,10 +90,13 @@ fun ItemListScreen(
                     }
                     // go through the items in the map and create a ListItem for each
                     items(it.value) { item ->
-                        ListItem(headlineContent = {
-                            Text("Item ${item.id} - ${item.name}")
-                        }
-                        )
+                        ListItem(
+                            headlineContent = {
+                                Text("Item ID:  ${item.id}")
+                            },
+                            supportingContent = {
+                                Text("Item Name:  ${item.name}")
+                            })
                     }
                 }
             }
